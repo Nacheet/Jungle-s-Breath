9c41c9c4d01735101c9d967012c06ea3ea481599
Unknown <ignasi.pelayo@enti.cat>
2018-03-07 19:45:07 +0100

    Moviment V0.1
    Canvi en el funcionament de les parets de les plataformes

---
+++ Jungle's Breath/Assets/PlayerController.cs
@@ -0,0 +1,234 @@
+﻿using UnityEngine;
+
+public class PlayerController: MonoBehaviour {
+
+    private GameObject player;
+
+    //Variables moviment
+    public LayerMask whatIsFloor;
+    public Transform groundCheck;
+    public float maxSpeed = 17;
+    public bool grounded = false;
+    private bool movingRight, movingLeft;
+
+    //Variables salt
+    public float jumpVelocity = 19.0f;
+    public float maxGravity = 7.0f, minGravity = 3.5f;
+    private bool falling;
+    //Variables salt paret
+    public Transform playerRight, playerLeft;
+    private bool slidingL, slidingR;
+    private bool wallJumping, jumpR, jumpL;
+    const float jumpXCap = 0.8f;
+    public float slidingGravity = 2.0f;
+
+    //Moviment del escut
+    public bool shieldRight, shieldLeft, shieldDown, shieldUp, shieldDefault;
+    public bool usingShield = false;
+    public float newSpeed;
+    private float maxSpeedCopy;
+
+
+    void Start ()
+    {
+        player = GameObject.Find("Player");
+        GetComponent<Rigidbody2D>().gravityScale = minGravity;
+
+        newSpeed = maxSpeed / 4;
+        maxSpeedCopy = maxSpeed;
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        //Comprova si esta al terra
+        grounded = Physics2D.Linecast(player.transform.position, groundCheck.position, whatIsFloor);
+        //Comprova si esta tocant la paret dreta o esquerra
+        slidingR = Physics2D.Linecast(player.transform.position, playerRight.position, whatIsFloor);
+        slidingL = Physics2D.Linecast(player.transform.position, playerLeft.position, whatIsFloor);
+
+        //Moviment lateral
+        move(Input.GetAxis("Horizontal"));
+
+        //Per saber si el jugador es mou cap a la dreta o cap a la esquerra
+        if (GetComponent<Rigidbody2D>().velocity.x < 0) 
+            movingLeft = true;
+        else if (GetComponent<Rigidbody2D>().velocity.x > 0)
+            movingRight = true;
+        else
+        {
+            movingRight = false;
+            movingLeft = false;
+        }
+
+        //Salt
+        if (Input.GetButtonDown("Jump"))
+            jump(grounded);
+        if (GetComponent<Rigidbody2D>().velocity.y < 0)
+        {
+            GetComponent<Rigidbody2D>().gravityScale = maxGravity;
+            falling = true;
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().gravityScale = minGravity;
+            falling = false;
+        }
+
+        //Salt desde la paret
+        if (slidingL && !grounded && Input.GetButtonDown("Jump"))
+        {
+            float xSpeed = maxSpeed * jumpXCap;
+            wallJumping = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xSpeed, jumpVelocity);
+            jumpR = true;
+
+        }
+
+        if (slidingR && !grounded && Input.GetButtonDown("Jump"))
+        {
+            float xSpeed = -maxSpeed * jumpXCap;
+            wallJumping = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(xSpeed, jumpVelocity);
+            jumpL = true;
+
+        }
+
+        if ((wallJumping) && GetComponent<Rigidbody2D>().velocity.y < 0)
+            wallJumping = false;
+        if (grounded || slidingR)
+            jumpR = false;
+        if (grounded || slidingL)
+            jumpL = false;
+
+        if ((slidingL || slidingR) && falling)
+            GetComponent<Rigidbody2D>().gravityScale = slidingGravity;
+
+
+        //Posicio del escut
+        if (Input.GetButton("Fire1"))
+        {
+            if (grounded)
+            {
+                GetComponent<PlayerController>().maxSpeed = newSpeed;
+                usingShield = true;
+            }
+            else
+                usingShield = true;
+        }
+        else
+        {
+            usingShield = false;
+            GetComponent<PlayerController>().maxSpeed = maxSpeedCopy;
+            shieldDefault = true;
+        }
+
+
+        shieldPosition(usingShield, movingRight, movingLeft);
+    }
+
+    private void move(float input)
+    {
+        if (wallJumping)
+            return;
+
+        Vector2 movement = GetComponent<Rigidbody2D>().velocity;
+        movement.x = input * maxSpeed; //Es modifica la velocitat de movement en el eix de les x tal que moviment rebut (input) * maxSpeed = M
+        GetComponent<Rigidbody2D>().velocity = movement; //La velocitat modificada s'aplica al GameObject, en aquest cas només modifica el eix de les x [(M,0)]
+
+        if(falling && jumpL && GetComponent<Rigidbody2D>().velocity.x == 0)
+        {
+            movement.x += maxSpeed / -3;
+            GetComponent<Rigidbody2D>().velocity = movement;
+        }
+        if(falling && jumpR && GetComponent<Rigidbody2D>().velocity.x == 0)
+        {
+            movement.x += maxSpeed / 3;
+            GetComponent<Rigidbody2D>().velocity = movement;
+        }
+    }
+
+    private void jump(bool grounded)
+    {
+        if (grounded)
+        {
+            GetComponent<Rigidbody2D>().velocity += jumpVelocity * Vector2.up; //Vector2.up = (0,1) Per tant, x*0 = 0 i x*1 = x [(0,x)]
+        }
+    }
+
+
+    void shieldPosition(bool usingShield, bool movingRight, bool movingLeft)
+    {
+        if (usingShield)
+        {
+            if (movingRight)
+            {
+                shieldRight = true;
+
+                shieldLeft = false;
+                shieldDown = false;
+                shieldUp = false;
+                shieldDefault = false;
+            }
+            else if (movingLeft)
+            {
+                shieldLeft = true;
+
+                shieldRight = false;
+                shieldDown = false;
+                shieldUp = false;
+                shieldDefault = false;
+            }
+            else if (Input.GetAxisRaw("Vertical") == 1)
+            {
+                shieldUp = true;
+
+                shieldRight = false;
+                shieldLeft = false;
+                shieldDown = false;
+                shieldDefault = false;
+            }
+
+            if (Input.GetAxisRaw("Vertical") == -1)
+            {
+                if (grounded)
+                {
+                    shieldDefault = true;
+
+                    shieldRight = false;
+                    shieldLeft = false;
+                    shieldDown = false;
+                    shieldUp = false;
+                }
+                else
+                {
+                    shieldDown = true;
+
+                    shieldRight = false;
+                    shieldLeft = false;
+                    shieldUp = false;
+                    shieldDefault = false;
+                }
+
+            }
+
+
+        }
+        else
+        {
+            shieldDefault = true;
+
+            shieldRight = false;
+            shieldLeft = false;
+            shieldDown = false;
+            shieldUp = false;
+        }
+
+
+    }
+
+}
+
+   
+
+
