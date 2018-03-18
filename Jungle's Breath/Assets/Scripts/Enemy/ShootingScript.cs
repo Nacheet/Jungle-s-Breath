dc64ec7bea29274cfb553e1d604cbe4d2e45df1e
Ares Fernández Ramos <ares.fernandez@enti.cat>
2018-03-18 16:39:50 +0100

    Shooting Script and Shots prefab partially implemented

---
+++ Jungle's Breath/Assets/Scripts/Enemy/ShootingScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ShootingScript : MonoBehaviour {
+
+    public GameObject shootingTarget;
+    public GameObject shot;
+    public Transform shotSpawn;
+    public Vector2 targetEnemyVector;
+    public float detectionDistance = 10;
+    public float fireRate = 1.0f;
+    private float nextShot = 0.0f;
+
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+
+    // Update is called once per frame
+    void Update() {
+
+        // Detección jugador
+        // Calculamos el vector entre el enemigo y el jugador
+        targetEnemyVector.x = shootingTarget.transform.position.x - transform.position.x;
+        targetEnemyVector.y = shootingTarget.transform.position.y - transform.position.y;
+
+        // Actuamos si el jugador entra en el rango de visión del enemigo
+        if (Mathf.Sqrt(Mathf.Pow(targetEnemyVector.x, 2) + Mathf.Pow(targetEnemyVector.y, 2)) <= detectionDistance)
+        {
+            // Disparo
+            if (Time.time>nextShot) // Disparamos si ha pasado el tiempo suficiente entre disparos
+            {
+                nextShot = Time.time + fireRate; // Ponemos el valor del tiempo del siguiente disparo
+                Instantiate<GameObject>(shot, shotSpawn.position, shotSpawn.rotation); // Instanciamos el disparo
+            }
+
+
+        }
+
+    }
+}
---
+++ Jungle's Breath/Assets/Scripts/Enemy/Shots/ShotBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotBehaviour : MonoBehaviour {
+
+    public float speed;
+    public Vector2 vector;
+    public float deathTime = 0.5f;
+    private float initTime;
+    private float modulo;
+
+
+    // Use this for initialization
+    void Start () {
+        vector = GameObject.Find("Enemy").GetComponent<ShootingScript>().targetEnemyVector;
+        modulo = Mathf.Sqrt(Mathf.Pow(vector.x, 2) + Mathf.Pow(vector.y, 2));
+
+        vector.x = (vector.x/modulo)*speed; // Hacemos que el vector sea unitario para quedarnos sólo con la dirección en la que queremos disparar
+        vector.y = (vector.y/modulo)*speed; // Lo multiplicamos por speed para añadirle la velocidad deseada
+
+        GetComponent<Rigidbody2D>().velocity=vector; // Aplicamos el vector resultante a la velocidad del objeto
+        initTime = Time.time;
+	}
+
+    private void Update()
+    {
+        if (Time.time-initTime>deathTime)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag=="Player")
+        {
+
+        }
+        else if (collision.gameObject.tag == "Enemy")
+        {
+
+        }
+    }
+}
