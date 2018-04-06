5de3da5d7922490bfae84fea77e3ad94889fb720
Unknown <ignasi.pelayo@enti.cat>
2018-04-06 18:18:10 +0200

    Eliminació del Tilemap i modificació del nivell Tutorial
    Modificació del nivell tutorial (jugable pero no acabat)
    Eliminació del tilemap (queda millor amb sprites sols)
    Ha aparegut un bug amb la caixa que cal solucionar

---
+++ Jungle's Breath/Assets/Scripts/Enemy/MovingEnemy.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyMovement : MonoBehaviour
+{
+
+
+
+    public GameObject startPoint;
+
+    public GameObject endPoint;
+
+
+
+    public float enemySpeed;
+
+    private bool rightDirection;
+
+    void Start()
+    {
+
+
+
+        if (!rightDirection)
+
+        {
+
+            transform.position = startPoint.transform.position;
+
+        }
+        else
+
+        {
+
+            transform.position = endPoint.transform.position;
+
+        }
+    }
+
+
+    void Update()
+    {
+
+        if (!rightDirection)
+
+        {
+
+            transform.position = Vector3.MoveTowards(transform.position, endPoint.transform.position, enemySpeed * Time.deltaTime);
+
+
+
+            if (transform.position == endPoint.transform.position)
+
+            {
+
+                rightDirection = true;
+
+                GetComponent<SpriteRenderer>().flipX = true;
+
+            }
+
+
+
+        }
+
+
+
+        if (rightDirection)
+
+        {
+
+            transform.position = Vector3.MoveTowards(transform.position, startPoint.transform.position, enemySpeed * Time.deltaTime);
+
+            if (transform.position == startPoint.transform.position)
+
+            {
+
+                rightDirection = false;
+
+                GetComponent<SpriteRenderer>().flipX = false;
+
+            }
+
+
+
+        }
+    }
+}
