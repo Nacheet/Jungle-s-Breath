3e958ceb2897c3d5e76d8d92031e08719d16a85c
Ares Fernández Ramos <ares.fernandez@enti.cat>
2018-04-16 19:51:58 +0200

    Menu v1

---
+++ Jungle's Breath/Assets/Scripts/Menu/ChangeScreen.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScreen : MonoBehaviour {
+
+	public void LoadSceneWithIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
