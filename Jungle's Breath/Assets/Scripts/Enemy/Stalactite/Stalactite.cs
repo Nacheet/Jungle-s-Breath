df8bd35107788dc619a9d432018ddd6117f6d4d7
Unknown <ignasi.pelayo@enti.cat>
2018-04-18 21:25:52 +0200

    Stalactite and enemy positioning
    "Enemic" stalactite creat
    Posicionament dels enemics del nivell 1

---
+++ Jungle's Breath/Assets/Scripts/Enemy/Stalactite/Stalactite.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stalactite : MonoBehaviour {
+
+    public GameObject detector;
+
+    int minGravity = 0;
+    public float maxGravity;
+    public bool playerUnder;
+    public bool hitPlayer;
+    public bool hitShield;
+    public float destroyTime;
+
+
+	void Update ()
+    {
+        playerUnder = detector.GetComponent<StalactiteDetector>().playerUnder;
+
+		if(playerUnder)
+        {
+            GetComponent<Rigidbody2D>().gravityScale = maxGravity;
+        }
+	}
+
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.collider.gameObject.tag == "Player")
+        {
+            hitPlayer = true;
+        }
+        else if (coll.collider.gameObject.tag == "Shield")
+        {
+            hitShield = true;
+        }
+        else if (coll.collider.gameObject.tag == "Ground")
+        {
+            Destroy(gameObject, destroyTime);
+        }
+    }
+
+}
---
+++ Jungle's Breath/Assets/Scripts/Enemy/Stalactite/StalactiteDetector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StalactiteDetector : MonoBehaviour {
+
+    public bool playerUnder;
+
+
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            playerUnder = true;
+        }
+    }
+}
