837217c4e7bec6ede50f9922ea2d85fe67823b8c
Unknown <ignasi.pelayo@enti.cat>
2018-05-15 18:51:07 +0200

    New Boss shot animation

---
+++ Jungle_s Breath/Assets/Scripts/Enemy/Shooting/ScaleProjectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleProjectile : MonoBehaviour {
+
+    private float scaleX, scaleY;
+
+	// Use this for initialization
+	void Start () {
+        scaleX = 0.9f;
+        scaleY = 0.9f;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (this.GetComponent<Rigidbody2D>().velocity.x < 0)
+            this.GetComponent<Transform>().localScale = new Vector2(-scaleX, scaleY);
+        else
+            this.GetComponent<Transform>().localScale = new Vector2(scaleX, scaleY);
+    }
+}
