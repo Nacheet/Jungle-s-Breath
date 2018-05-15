c26c7730881edc001c1a90947a82f0b6b3d6b302
Unknown <ignasi.pelayo@enti.cat>
2018-05-15 19:38:15 +0200

    Added Menu Music and some sound effects and a controller
    Added a calm music at the beggining and a danger sound when "Play Game"

--- Jungle_s Breath/Assets/Scripts/Menu/MainMenu.cs
+++ Jungle_s Breath/Assets/Scripts/Menu/MainMenu.cs
@@ -2,8 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
 
 public class MainMenu : MonoBehaviour {
+
+    private PlayMusicChange musicChanger;
+    public AudioMixer main;
+    private bool start;
+    private float time;
+    public float timeToStart;
+
+    void Start()
+    {
+        musicChanger = GameObject.Find("Canvas").GetComponent<PlayMusicChange>();
+    }
+
+    void Update()
+    {
+        if(start && Time.time < time + timeToStart)
+        {
+            main.SetFloat("volume", -20);
+        }
+        else if(start && Time.time > time + timeToStart)
+        {
+            main.SetFloat("volume", 0);
+            musicChanger.ChangeAudioClip();
+            playGame();
+        }
+    }
+
+
+    public void startCounter()
+    {
+        start = true;
+        time = Time.time;
+    }
 
     public void playGame()
     {
